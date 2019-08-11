5e299ab7260a3e25dc931c6bffbeaf84e7c1cc6d
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2019-08-11 13:15:49 +0100

    added some metadata

--- setup.py
+++ setup.py
@@ -2,6 +2,9 @@ from setuptools import setup
 
 setup(
     name = 'TomoS2N',
+    version = '1.0'
+    author = 'Auggie Marignier',
+    author_email = 'augustin.marignier.14@ucl.ac.uk',
     packages = ['src'],
     package_data = {'src':['masks/*']},
     entry_points = {'console_scripts':[
