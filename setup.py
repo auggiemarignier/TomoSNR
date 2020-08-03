075b25b10080b9f31b077830b977c64eaaf5a1c0
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2020-08-03 17:20:03 +0100

    rename main file

--- setup.py
+++ setup.py
@@ -1,13 +1,11 @@
 from setuptools import setup
 
 setup(
-    name = 'TomoSNR',
-    version = '1.2',
-    author = 'Auggie Marignier',
-    author_email = 'augustin.marignier.14@ucl.ac.uk',
-    packages = ['src'],
-    package_data = {'src':['masks/*']},
-    entry_points = {'console_scripts':[
-        'tomosnr = src.command:process'
-    ]}
+    name="TomoSNR",
+    version="1.2",
+    author="Auggie Marignier",
+    author_email="augustin.marignier.14@ucl.ac.uk",
+    packages=["src"],
+    package_data={"src": ["masks/*"]},
+    entry_points={"console_scripts": ["tomosnr = src.main:process"]},
 )
